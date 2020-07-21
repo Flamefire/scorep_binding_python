bd018b116e538a829ad68f83496432787ee62010
Alexander Grund <Alexander.Grund@tu-dresden.de>
2020-07-21 11:02:42 +0200

    [SQUASHME] Fix previous commit

--- src/scorepy/pythonHelpers.hpp
+++ src/scorepy/pythonHelpers.hpp
@@ -75,13 +75,14 @@ auto cast_to_PyFunc(TFunc* func) -> detail::ReplaceArgsToPyObject_t<TFunc>*
 
 inline CString get_string_from_python(PyObject& o)
 {
-    Py_ssize_t len;
 #if PY_MAJOR_VERSION >= 3
+    Py_ssize_t len;
     const char* s = PyUnicode_AsUTF8AndSize(&o, &len);
-#else
-    const char* s = PyString_FromStringAndSize(&o, &len);
-#endif
     return CString(s, len);
+#else
+    const char* s = PyString_AsString(&o);
+    return CString(s);
+#endif
 }
 
 /// Pair of a C-String and it's length useful for PyArg_ParseTuple with 's#'
